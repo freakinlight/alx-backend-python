444f05e9a75864ffcf3407402c970d5fe502a44f
Light <mazouznour0@gmail.com>
2024-10-14 10:37:31 +0100

    fixed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-This module defines a coroutine that spawns multiple wait_random coroutines
-and returns a list of delays in ascending order.
+This module defines a coroutine that spawns multiple wait_random
+coroutines and returns a list of delays in ascending order.
 """
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+from basic_async_syntax import wait_random  # Updated import statement
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:

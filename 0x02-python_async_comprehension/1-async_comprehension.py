22186fc72be85a7e78cb8006df746bf2add2cae9
Light <mazouznour0@gmail.com>
2024-10-15 15:08:12 +0100

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Module uses async comprehensions to collect values from an asynchronous
+generator.
+"""
+
+import asyncio
+from typing import List
+
+# Adjust the import if necessary depending on your project structure
+from async_generator import async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers from
+    async_generator using async comprehension.
+    Returns:
+        List[float]: List of 10 random numbers.
+    """
+    return [i async for i in async_generator()]
+
+
+async def main():
+    print(await async_comprehension())
+
+if __name__ == '__main__':
+    asyncio.run(main())
